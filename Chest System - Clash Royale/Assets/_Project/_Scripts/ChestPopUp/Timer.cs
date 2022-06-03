1894874f41786c0d0b0a2762f97f90411b6421ed
arunprksm <97307361+arunprksm@users.noreply.github.com>
2022-06-03 07:09:18 +0530

    Timer Function Initial check...

--- Chest System - Clash Royale/Assets/_Project/_Scripts/ChestPopUp/Timer.cs
+++ Chest System - Clash Royale/Assets/_Project/_Scripts/ChestPopUp/Timer.cs
@@ -6,41 +6,36 @@ using UnityEngine.UI;
 
 public class Timer : SingletonGenerics<Timer>
 {
-    public float timeValue;
-    //public Text timerText;
-    public ChestView ChestView;
-    public static event Action OnTimer;
-    public static event Action<ChestView, float> OnTimerDisplay;
-    public void Update()
+    public Button chestButton;
+    private ulong lastChestOpen;
+    private void Start()
     {
-        OnTimer?.Invoke();
-        OnTimerDisplay?.Invoke(ChestView, timeValue);
-        //DisplayTime(ChestView, timeValue);
+        PlayerPrefs.DeleteAll();
+        chestButton.interactable = true;
+
+        //if (lastChestOpen != 0)
+        //lastChestOpen = ulong.Parse(PlayerPrefs.GetString("LastChestOpen"));
+
+    }
+    private void Update()
+    {
+        if (!chestButton.IsInteractable())
+        {
+            ulong diff = ((ulong)DateTime.Now.Ticks - lastChestOpen);
+            ulong m = (ulong)diff / TimeSpan.TicksPerMillisecond;
+
+            //float secondsLeft = (float)(3000 - m) / 1000;
+            //if (secondsLeft < 0)
+            //{
+
+            //}
+        }
     }
 
-    public void TimerFunction()
+    public void OnClick()
     {
-        if (timeValue > 0)
-        {
-            timeValue -= Time.deltaTime;
-        }
-        else
-        {
-            timeValue = 0;
-        }
-    }
-
-
-    public void DisplayTime(ChestView ChestView, float timeToDisplay)
-    {
-        if (timeToDisplay < 0)
-        {
-            timeToDisplay = 0;
-        }
-
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-
-        ChestView.TimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        lastChestOpen = (ulong)DateTime.Now.Ticks;
+        PlayerPrefs.SetString("LastChestOpen", lastChestOpen.ToString());
+        chestButton.interactable = false;
     }
 }
